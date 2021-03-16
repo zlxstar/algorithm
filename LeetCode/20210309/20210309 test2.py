9b4d8d977931603996c2ce21e64007c6dc6467d7
zlxstar <522640402@qq.com>
2021-03-16 14:34:14 +0800

    first commit

--- LeetCode/20210309/20210309 test2.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def restoreString(self, s: str, indices: List[int]) -> str:
-        last_s = ''
-        for i in range(len(indices)):
-            last_s = last_s + s[indices.index(i)];
-        
-        return last_s
